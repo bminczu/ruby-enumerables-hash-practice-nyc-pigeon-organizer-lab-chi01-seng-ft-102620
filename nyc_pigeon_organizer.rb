8b076d68507b0b5f984946ed37ba6201e327790c
adventurous-format-6947 <bminczu@gmail.com>
2020-10-12 17:20:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object ({}) do |(key,value), final_array|# write your code here!
   binding.pry
   value.each do |name|
-      binding.pry
+      
     if !final_array[name]
       final_array[name]={}
     end
